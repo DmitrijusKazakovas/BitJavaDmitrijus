637384729d2df94665cd8fda090d25781e7f2b06
dmitrijusKazakovas101 <dmitrijus.kazakovas@yandex.com>
2020-09-02 15:17:36 +0300

    Pirmas komitas

---
+++ src/com/dmitrijus/app/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.dmitrijus.app;
+
+public class HelloWorld {
+    public static void main (String[] args){
+        System.out.println("Goodbye, world.");
+        System.out.println("Goodbye, world.");
+    }
+}
